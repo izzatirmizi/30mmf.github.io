470f77ca4ba2a14a120ce375fcedbd71812d407f
izzatirmizi <iat2632@nyu.edu>
2025-09-18 02:30:00 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+let currentIndex = 0;
+
+function showSlide(index) {
+  const track = document.querySelector(".carousel-track");
+  const slides = document.querySelectorAll(".carousel-track img");
+  const totalSlides = slides.length;
+
+  if (index >= totalSlides) {
+    currentIndex = 0;
+  } else if (index < 0) {
+    currentIndex = totalSlides - 1;
+  } else {
+    currentIndex = index;
+  }
+
+  const offset = -currentIndex * 100;
+  track.style.transform = `translateX(${offset}%)`;
+}
+
+function nextSlide() {
+  showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+  showSlide(currentIndex - 1);
+}
